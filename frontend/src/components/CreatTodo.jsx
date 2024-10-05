f7f746f634e3509f09eb4fb9665f78871c22b4a2
root <ahmedalgohary1170@gmail.com>
2024-10-05 05:20:59 +0300

    design todo UI

--- frontend/src/components/CreatTodo.jsx
+++ frontend/src/components/CreatTodo.jsx
@@ -8,9 +8,26 @@ const CreatToDo = () => {
 
 
     return(
-        <div>
-            <h3>Create ToDo</h3>
-        </div>
+       <div className="container">
+            <form className="row w-120">
+                <div className="col-7">
+                    <input type="text" className="form-control" placeholder="Enter Todo Title" />
+                </div>
+                <div className="col-2">
+                    <select className="form-control">
+                        <option value="DONE">DONE</option>
+                        <option value="INPROGRESS">InProgress</option>
+
+                    </select>
+               
+                </div>
+
+                <div className="col-3">
+                    <button type="submit" className="btn btn-primary"> Add ToDo </button>
+                </div>
+
+            </form>
+       </div>
     )
 
 
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 
 
 
@@ -5,12 +7,25 @@
 
 
 const Todos = () => {
-
+    const [todos,settodos] = useState([
+        {title:'todos1',status:'INPROGRESS'},
+        {title:'todos2',status:'DONE'},
+        {title:'todos3',status:'INPROGRESS'},
+        {title:'todos4',status:'DONE'},
+    ])
 
 
     return(
-        <div>
-            <h3>ToDos</h3>
+        <div className="container mt-4">
+            <ul className="list-group">
+                {todos.map((todo,index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between"> 
+                        <span>{todo.title}</span>
+                        <button className="btn btn-sm border">{todo.status}</button>
+                    </li>
+
+                ))}
+            </ul>
         </div>
     )
 }
