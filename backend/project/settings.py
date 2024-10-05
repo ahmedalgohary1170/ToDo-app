0dbcce20450c0a1b7400f9d62aabd90c7a30700d
root <ahmedalgohary1170@gmail.com>
2024-10-05 15:51:39 +0300

    get todos from API

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,19 +37,32 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    "corsheaders",
     'rest_framework',
+    
+    
     'todo',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Cors
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRS_ALLOWED_ORIGINS = ['http://localhost:5173/']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173/']
+
 
 ROOT_URLCONF = 'project.urls'
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react'
+import {useEffect,useState } from 'react'
 import './App.css'
 import Todos from './components/Todos';
 import CreatToDo from './components/CreatTodo';
@@ -10,13 +10,12 @@ import CreatToDo from './components/CreatTodo';
 
 
 function App() {
-  const [todos,settodos] = useState([
-    {title:'todos1',status:'INPROGRESS'},
-    {title:'todos2',status:'DONE'},
-    {title:'todos3',status:'INPROGRESS'},
-    {title:'todos4',status:'DONE'},
-  ])
-
+  const [todos,settodos] = useState([])
+  useEffect (()=>{
+    fetch("http://127.0.0.1:8000/todo/")
+    .then(response => response.json())
+    .then(data => settodos(data))
+  })
 
   return (
   <div className='container'>
